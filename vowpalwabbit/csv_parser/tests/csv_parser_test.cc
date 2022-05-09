f12a0a50810e5c463a90940f484de07516c3d473
Hollow Man <hollowman@opensuse.org>
2022-05-09 13:33:12 +0800

    feat: allow to use quotes to specify feature values as strings instead of judge only based on the content
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- vowpalwabbit/csv_parser/tests/csv_parser_test.cc
+++ vowpalwabbit/csv_parser/tests/csv_parser_test.cc
@@ -2,14 +2,4 @@
 // individual contributors. All rights reserved. Released under a BSD (revised)
 // license as described in the file LICENSE.
 
-// #include "vw/core/constant.h"
-// #include "vw/core/feature_group.h"
-// #include "vw/core/parse_example.h"
-// #include "vw/core/vw.h"
-// #include "vw/csv_parser/parse_example_csv.h"
-
-// #include <gmock/gmock.h>
-// #include <gtest/gtest.h>
-
-// #include <string>
-// #include <vector>
+// TODO: Test cases for the CSV parser
